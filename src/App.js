94bbc1b708d85f1ffced5fdfc47b3bda9c4d1d44
Jacolby Green <darknightnov@gmail.com>
2018-01-30 12:26:25 -0600

    first commit -- added app component for header and navigation

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Home from './Home.js'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="mdl-layout mdl-js-layout">
+          <header className="demo-layout-transparent mdl-color--primary">
+              <div className="mdl-layout-icon"></div>
+              <div className="mdl-layout__header-row">
+                <span className="mdl-layout__title">Jacolby Green</span>
+                <div className="mdl-layout-spacer"></div>
+                <nav className="mdl-navigation mdl-layout--large-screen-only">
+                    <a className="mdl-navigation__link" href="index.html">Home</a>
+                    <a className="mdl-navigation__link" href="portfolio.html">Portfolio</a>
+                    <a className="mdl-navigation__link" href="contact.html">Contact</a>
+                    <a className="mdl-navigation__link" href="https://github.com/darknight1983">Github</a>
+                  </nav>
+              </div>
+            </header>
+            <div className="mdl-layout__drawer">
+              <span className="mdl-layout-title">Jacolby Green</span>
+              <nav className="mdl-navigation">
+                <a className="mdl-navigation__link" href="index.html">Home</a>
+                <a className="mdl-navigation__link" href="portfolio.html">Portfolio</a>
+                <a className="mdl-navigation__link" href="contact.html">Contact</a>
+                <a className="mdl-navigation__link" href="https://github.com/darknight1983">Github</a>
+              </nav>
+            </div>
+            <main className="mdl-layout__content">
+
+                <Home />
+
+
+                <footer className="mdl-mini-footer mdl-color--grey-900">
+                  <div className="mdl-mini-footer__middle-section">
+                    <button className="mdl-mini-footer--social-btn social-btn social-btn__twitter" id="twitter">
+                      <span className="visuallyhidden">Twitter</span>
+                    </button>
+                    <button className="mdl-mini-footer--social-btn social-btn social-btn__facebook" id="facebook">
+                      <span className="visuallyhidden">Facebook</span>
+                    </button>
+                    <button className="mdl-mini-footer--social-btn social-btn social-btn__linkedin" id="linkedin">
+                      <span className="visuallyhidden">Linkedin</span>
+                    </button>
+                  </div>
+                </footer>
+              </main>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+            introduction: `I fell in love with technology after the entrance of the
+                          iPhone in 2007. I had already had a general interest with tech prior
+                          to the iPhone’s release but after witnessing what could be done with
+                          tech, I was hooked! That led me to work at companies like
+                          AT&amp;T and Mosaic to quench my thirst for all things
+                          tech. That passion eventually lead me on a journey to learn
+                          to learn how to code. I recently finished a 3 month
+                          bootcamp in Web Development and I'm now looking to build
+                          robust applications with a fast-paced and growing company.`,
+            currently: `I am currently attending SMU's Full Stack Flex Web Development bootcamp
+                   which is a 6 month program aimed at preparing students to be front-end and
+                   back-end developers. I am also a recipient of "Grow with Goole Challenge"
+                   Scholarship which means that I get to interact with people all over the world
+                   to participate in global learning environment. My goal is to code as much as I
+                   possible can until Im in a position to do some good.`
+    }
+  }
+  render() {
+    return (
+      <div className="mdl-grid portfolio-max-width">
+        <div className="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text">Welcome</h2>
+          </div>
+          <div className="mdl-card__media">
+            <img className="article-image" src="assets/images/about_me.jpg" border="0" alt=""/>
+          </div>
+          <div className="mdl-grid portfolio-copy">
+            <h3 className="mdl-cell mdl-cell--12-col mdl-typography--headline">Introduction</h3>
+            <div className="mdl-cell mdl-cell--10-col mdl-card__supporting-text no-padding">
+              <p>
+                {this.state.introduction}
+              </p>
+            </div>
+            <h3 className="mdl-cell mdl-cell--12-col mdl-typography--headline">Currently</h3>
+            <div className="mdl-cell mdl-cell--10-col mdl-card__supporting-text no-padding">
+              <p>
+                {this.state.currently}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
