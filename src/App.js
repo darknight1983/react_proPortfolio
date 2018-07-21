f3beae2ab5b97eb0d10beb08b13eecd10c202a30
Jacolby Green <darknightnov@gmail.com>
2018-07-21 17:48:34 -0500

    In the process of adding BlogPost 4 to site.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
                     <Link to="/blog"><a className="mdl-navigation__link" href="blog.html">Blog</a></Link>
                     <Link to='/portfolio'><a className="mdl-navigation__link" href="portfolio.html">Portfolio</a></Link>
                     <Link to='/contact'><a className="mdl-navigation__link" href="contact.html">Contact</a></Link>
-                    <a className="mdl-navigation__link" href="https://github.com/darknight1983">Github</a>
+                    <a className="mdl-navigation__link" href="https://github.com/darknight1983" target="_blank">Github</a>
                     </nav>
                 </div>
               </header>
@@ -36,7 +36,7 @@ class App extends Component {
                   <Link to="/blog"><a className="mdl-navigation__link" href="blog.html">Blog</a></Link>
                   <Link to='/portfolio'><a className="mdl-navigation__link" href="portfolio.html">Portfolio</a></Link>
                   <Link to='/contact'><a className="mdl-navigation__link" href="contact.html">Contact</a></Link>
-                  <a className="mdl-navigation__link" href="https://github.com/darknight1983">Github</a>
+                  <a className="mdl-navigation__link" href="https://github.com/darknight1983" target="_blank">Github</a>
                 </nav>
               </div>
               <main className="mdl-layout__content">
@@ -55,13 +55,13 @@ class App extends Component {
 
                   <footer className="mdl-mini-footer mdl-color--grey-900">
                     <div className="mdl-mini-footer__middle-section">
-                      <button onClick={() => {window.location.href = 'https://twitter.com/CoodyMac'}} className="mdl-mini-footer--social-btn social-btn social-btn__twitter" id="twitter">
+                      <button onClick={() => window.location.href = 'https://twitter.com/CoodyMac'} className="mdl-mini-footer--social-btn social-btn social-btn__twitter" id="twitter">
                         <span className="visuallyhidden">Twitter</span>
                       </button>
-                      <button onClick={() => {window.location.href = 'https://www.facebook.com/jacolby.green'}} className="mdl-mini-footer--social-btn social-btn social-btn__facebook" id="facebook">
+                      <button onClick={() => window.location.href = 'https://www.facebook.com/jacolby.green'} className="mdl-mini-footer--social-btn social-btn social-btn__facebook" id="facebook">
                         <span className="visuallyhidden">Facebook</span>
                       </button>
-                      <button onClick={() => {window.location.href = 'https://www.linkedin.com/in/jacolby-green-308b701b'}} className="mdl-mini-footer--social-btn social-btn social-btn__linkedin" id="linkedin">
+                      <button onClick={() => window.location.href = 'https://www.linkedin.com/in/jacolby-green-308b701b'} className="mdl-mini-footer--social-btn social-btn social-btn__linkedin" id="linkedin">
                         <span className="visuallyhidden">Linkedin</span>
                       </button>
                     </div>
---
+++ src/blogContent4.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class BlogContent4 extends Component {
+  render{
+    return (
+      
+    )
+  }
+}
